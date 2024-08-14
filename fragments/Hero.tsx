a4bf0b183a0d434d802585c76416662ee3d8aa6b
JulianKominovic <juliankominovic@gmail.com>
2024-08-14 00:09:27 -0300

    feat: gallery

---
+++ fragments/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import GradientSection from "@/components/GradientSection";
+
+type Props = {};
+
+const Hero = () => {
+  return <Header />;
+};
+
+export default Hero;
---
+++ initial.ts
@@ -0,0 +1,55 @@
+import fs from "fs/promises";
+export let WALLPAPERS_COUNT = 0;
+export let WALLPAPERS_INDEX: WallpaperFolder = null!;
+
+type WallpaperFolder = {
+  anime: {
+    "dark-skies": string[];
+    "reddish-outerspace": string[];
+    "snowy-mountains": string[];
+    waterfalls: string[];
+    "villages-noontime": string[];
+    "villages-sunset": string[];
+  };
+  "cute-things": {
+    keyboards: string[];
+  };
+};
+export async function initWallpapers() {
+  if (WALLPAPERS_INDEX) return;
+  const categories = await fs.readdir("./public/wallpapers");
+  WALLPAPERS_INDEX = {
+    anime: {
+      "dark-skies": [],
+      "reddish-outerspace": [],
+      "snowy-mountains": [],
+      waterfalls: [],
+      "villages-noontime": [],
+      "villages-sunset": [],
+    },
+    "cute-things": {
+      keyboards: [],
+    },
+  };
+  await Promise.all(
+    categories.map(async (category) => {
+      const files = await fs.readdir(`./public/wallpapers/${category}`);
+      return await Promise.all(
+        files.map(async (subcategory) => {
+          const files = await fs.readdir(
+            `./public/wallpapers/${category}/${subcategory}`
+          );
+          files.forEach((f) => {
+            // @ts-ignore
+            WALLPAPERS_INDEX[category][subcategory].push(
+              `/wallpapers/${category}/${subcategory}/${f}`
+            );
+            WALLPAPERS_COUNT++;
+          });
+        })
+      );
+    })
+  );
+}
+
+await initWallpapers();
